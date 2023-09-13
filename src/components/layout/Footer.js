27475cea158e6ccdb208c0a605e44ecbfe128ea3
joaopcaldeira <joaopccaldeira1999@gmail.com>
2023-09-13 18:44:33 -0300

    foot links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,21 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+import { Link } from 'react-router-dom'
+
+
 function Footer() {
-    return <p>Footer</p>
+    return (<footer className={styles.footer}>
+        <ul className={styles.social_list}>
+            <Link to="https:www.linkedin.com/in/joaopedrocaldeira/">
+                <FaLinkedin />
+            </Link>
+            <Link to="https://github.com/joaopcaldeira">
+                <FaGithub />
+            </Link>
+        </ul>
+        <p className={styles.copy_right}><span>RPG System</span> &copy; 2023</p>
+    </footer>)
 }
 
 export default Footer
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,19 +8,19 @@ function Navbar() {
         <nav class={styles.navbar}>
             <Container>
                 <Link to="/">
-                    <img src={logo} alt="d20" />
+                    <img src={logo} alt="d20" width={51} height={51} />
                 </Link>
-                <ul class={styles.list}>
-                    <li>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
                         <Link to='/'>Home</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/charselection'>CharSelection</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/newchar'>NewChar</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/register'>Register</Link>
                     </li>
                 </ul>
